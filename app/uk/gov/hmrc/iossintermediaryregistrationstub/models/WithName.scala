63d1a5c230ed204fe3d8d9131ad464eeff2db1f1
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2025-04-17 17:05:26 +0100

    VEI-140 | Add auth entities and VAT details entities.

--- app/uk/gov/hmrc/iossintermediaryregistrationstub/models/WithName.scala
+++ app/uk/gov/hmrc/iossintermediaryregistrationstub/models/WithName.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,18 +14,10 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.iossintermediaryregistrationstub.controllers
+package uk.gov.hmrc.iossintermediaryregistrationstub.models
 
-import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
-import play.api.mvc.{Action, AnyContent, ControllerComponents}
-import javax.inject.{Inject, Singleton}
-import scala.concurrent.Future
-
-@Singleton()
-class MicroserviceHelloWorldController @Inject()(cc: ControllerComponents)
-    extends BackendController(cc) {
-
-  def hello(): Action[AnyContent] = Action.async { implicit request =>
-    Future.successful(Ok("Hello world"))
-  }
+class WithName(string: String) {
+  override val toString: String = string
 }
+
+
